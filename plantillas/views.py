18a5edf9bf726ab0f36c3d8e34f8df8636c15f86
cricros <vce11987@innovaccion.mx>
2023-08-01 11:53:06 -0600

    dynamic templates

--- plantillas/views.py
+++ plantillas/views.py
@@ -1,11 +1,9 @@
 # para hacer el render de las vistas (templates)
 from django.shortcuts import render
 
-def simple(request):
-    return render(request, 'simple.html',{})
-asdadasd
-aaaaaaaaaaaaaaaaaaaaaaaaaa
+def simple(request, name):
+    return render(request, 'simple.html',{}) #aqui va el contexto
 
-asdadasd
-
-asdasdasdasdasdsa
+def dinamico(request, name):
+    context = {'name': name}
+    return render(request, 'dinamico.html',context)
